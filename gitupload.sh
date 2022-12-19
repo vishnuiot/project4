9286926e75fb4c960d6d53855579f80900c27dba
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-19 12:36:37 +0100

    file_modification

--- gitupload.sh
+++ gitupload.sh
@@ -2,3 +2,5 @@ git init
 # git commit -m "modification"
 # # git@github.com:vishnuiot/project4.git
 # git clone git@github.com:vishnuiot/project4.git
+
+#  git remote add origin git@github.com:vishnuiot/project4.git
