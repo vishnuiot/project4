37a8563346a921dbb55305b4c15f6dbe2417820f
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-19 23:26:10 +0100

    modification

--- 3.program.c
+++ 3.program.c
@@ -1,13 +1,13 @@
-//Example for short
+//Example for short memory space, long memory space
 
 #include <stdio.h>
 int main()
 {
-    unsigned char costA=300;
+    unsigned long costA=300; //change char to long to accomodate extra memory space
     unsigned char costB=100;
     unsigned long total;
     total=costA+costB;
-    printf("char size is %d",sizeof(char));
+    printf("char size is %d \n",sizeof(long));
     printf("The total cost is %ld \n",total);
     
 }
