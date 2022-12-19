c0c87f462e31fe2dcefd2fcb2aa3b8764d95699d
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-19 17:20:27 +0100

    modification

--- gitupload.sh
+++ gitupload.sh
@@ -1,11 +1,7 @@
 # git init . // only for brand new projects
 git add .    # all files
 git commit -m "modification"
-# # git@github.com:vishnuiot/project4.git
-# git clone git@github.com:vishnuiot/project4.git
-
-#  
 git remote add origin git@github.com:vishnuiot/project4.git
-git branch -M main
-git branch
+git pull origin master
+git push origin master
 
