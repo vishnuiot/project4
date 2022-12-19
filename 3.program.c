7328d273893e48d1d65e679626091cac9cefcaf1
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-19 23:15:49 +0100

    modification

---
+++ 3.program.c
@@ -0,0 +1,12 @@
+//Example for short
+
+#include <stdio.h>
+int main()
+{
+    unsigned char costA=30;
+    unsigned char costB=100;
+    unsigned short total;
+    total=costA+costB;
+    printf("The total cost is %d \n",total);
+    
+}
