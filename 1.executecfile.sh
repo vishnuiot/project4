e3b439245320414345e107215d3dd35504dc29f3
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-19 23:10:05 +0100

    modification

--- 1.executecfile.sh
+++ 1.executecfile.sh
@@ -1,2 +1,3 @@
-gcc -o test 1.program.c
+#gcc -o test 1.program.c
+gcc -o test 2.program.c
 ./gitupload.sh
---
+++ 2.program.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    unsigned char costA=20;
+    unsigned char costB=100;
+    unsigned char total;
+    total=costA+costB;
+    printf("The total cost is %d \n",total);
+}
