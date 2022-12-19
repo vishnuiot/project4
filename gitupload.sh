d7bf0286e1bf1778f4988b19d0eabf53bf9b2d12
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-19 17:28:19 +0100

    Delete gitupload.sh
    delete

--- gitupload.sh
+++
@@ -1,7 +0,0 @@
-# git init . // only for brand new projects
-git add .    # all files
-git commit -m "modification"
-git remote add origin git@github.com:vishnuiot/project4.git
-git pull origin main
-git push origin main
-
