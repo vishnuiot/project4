ecf819d804368f9d988812a446501f758a4ef7a2
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-19 23:23:08 +0100

    modification

--- 3.program.c
+++ 3.program.c
@@ -5,8 +5,9 @@ int main()
 {
     unsigned char costA=300;
     unsigned char costB=100;
-    unsigned short total;
+    unsigned long total;
     total=costA+costB;
+    printf("char size is %d",sizeof(char));
     printf("The total cost is %ld \n",total);
     
 }
