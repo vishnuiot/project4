ea16d6a8b8e30bc287dbd499142bd73abfac4b33
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-19 23:12:18 +0100

    modification

--- 2.program.c
+++ 2.program.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    unsigned char costA=20;
+    unsigned char costA=30;
     unsigned char costB=100;
     unsigned char total;
     total=costA+costB;
     printf("The total cost is %d \n",total);
+    
 }
