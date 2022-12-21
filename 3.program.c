0dade94ff91cce6af363548364d35e30352b74d0
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-21 01:30:20 +0100

    modification

--- 3.program.c
+++ 3.program.c
@@ -11,6 +11,7 @@ int main()
     printf("The total cost is %ld \n",total);
     printf("The size of char is %ld \n",sizeof (char));
     printf("The of unsigned char is %d \n",sizeof (unsigned char));
-    printf("The size of int is %d \n",sizeof (int));
+    printf("The size of int is %ld \n",sizeof (int));
+    printf("The size of float is %ld \n",sizeof (float));
     
 }
