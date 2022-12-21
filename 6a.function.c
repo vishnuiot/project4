14a75b1a7af917323e392b505d637129f3dd3c88
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-21 23:28:04 +0100

    modification

--- 6a.function.c
+++ 6a.function.c
@@ -1,5 +1,7 @@
 
-//function declaration
+//external function declaration - compile along with the calling function
+// gcc 6.program.c 6a.function.c -o test
+
 int add(int a, int b)
 {
     int result = a+b;
