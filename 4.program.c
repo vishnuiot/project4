d290fab6734f28568fb988d91948c3fce5d77631
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-21 01:39:45 +0100

    modification

--- 4.program.c
+++ 4.program.c
@@ -2,9 +2,12 @@
 
 #include <stdio.h>
 int a=5;
+
 int main()
 {
+unsigned long int addressofA=&a;
 printf("The address of a is %p \n",&a);
+printf("The address of a %ld  \n",addressofA);
 return 0;
 
 }
