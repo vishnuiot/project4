0a6cf6765e8a802b168cdc079f3786b11515d70f
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-23 00:00:28 +0100

    modification

--- 1.executecfile.sh
+++ 1.executecfile.sh
@@ -5,8 +5,8 @@
 #gcc -o test 5.program.c
 #gcc -o test 6.program.c 6a.function.c 
 #gcc -o test 7.program.c 7a.function.c
-gcc -o test 9.program.c
-#gcc -o test 10.program.c
+#gcc -o test 9.program.c
+gcc -o test 10.program.c
 ./gitupload.sh
 
 # execute using ./test
---
+++ 10.program.c
@@ -0,0 +1,10 @@
+// increment  a pointer and how it works
+
+#include <stdio.h>
+
+int main ()
+{
+
+    printf("test program");
+    return 0;
+}
