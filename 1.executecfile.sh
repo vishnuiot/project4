dbf7aa306e2d2e2cf75deaa8a551f15766123799
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-22 23:37:41 +0100

    modification

--- 1.executecfile.sh
+++ 1.executecfile.sh
@@ -4,7 +4,8 @@
 #gcc -o test 4.program.c
 #gcc -o test 5.program.c
 #gcc -o test 6.program.c 6a.function.c 
-gcc -o test 7.program.c 7a.function.c
+#gcc -o test 7.program.c 7a.function.c
+gcc -o test 9.program.c
 ./gitupload.sh
 
 # execute using ./test
--- 8.program.c
+++ 8.program.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+void addnumbers(int a,int b); // adds two values
+//void sub() // subtracts two values
 
 int main()
 {
-    printf("test \n");
+//    printf("test \n");
+    addnumbers(5,6);
+    printf("addition of two numbers = %d \n",addnumbers);
+    return 0;
+}
+
+void addnumbers(int a,int b)
+{
+    return a+b;
 }
