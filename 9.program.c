99985df03e575f7c9bfe3d153f15a352524f1c93
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-22 23:57:13 +0100

    modification

--- 9.program.c
+++ 9.program.c
@@ -1,4 +1,4 @@
-//Example for pointer variables
+//Example for pointer variables - read operation
 // create a char variable and initialize the value as 1500
 // print the address of the data 
 // create a pointer variable
@@ -11,12 +11,15 @@
 int main ()
 {
     char data =100;
-    // printf (" The addrress of the variable data is %x \n ",&data);// %x format specifier hexadecimal
     char *pointervariable; 
     pointervariable=&data;
+    // read operation
     char value=*pointervariable; // dereference the pointer variable
     printf("The address of the data %x \n",&data);
     printf(" The value stored in pointer variable is  %d  \n",value);
-    // printf ("test program \n");
+
+    // write operation
+    *pointervariable =60;
+    printf(" The value stored in pointer variable is  %d  \n",data);
     return 0;
 }
