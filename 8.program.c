570db24eaec15a824bbb8588452f4f2d898e8e94
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-24 22:31:10 +0100

    modification

--- 8.program.c
+++ 8.program.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 void addnumbers(int a,int b); // adds two values
-void mulnumbers(int a,int b); // multiply two values
+// void mulnumbers(int a,int b); // multiply two values
 //void sub() // subtracts two values
 
 int main()
@@ -10,17 +10,17 @@ int main()
 //    printf("test \n");
     addnumbers(5,6);
     printf("addition of two numbers = %d \n",addnumbers);
-    mulnumbers(5,10);
-    printf("The multiplication of two numbers = %d \n",mulnumbers);
+    // mulnumbers(5,10);
+    // printf("The multiplication of two numbers = %d \n",mulnumbers);
     return 0;
 }
 
-void addnumbers(int a,int b)
+int addnumbers(int a,int b);
 {
     return a+b;
 }
 
-void mulnumbers(int a,int b)
-{
-    return a*b;
-}
+// int mulnumbers(int a,int b);
+// {
+//     return a*b;
+// }
