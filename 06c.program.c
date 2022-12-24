8a08bca8b0aa8d30c3cde97724e7482d4cc407b7
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-25 00:13:21 +0100

    modification

--- 06c.program.c
+++ 06c.program.c
@@ -6,10 +6,40 @@
 int mathadd(int a, int b);
 int mathmul(int a,int b);
 int mathsub(int a,int b);
-int mathdivide(int a, int b);
+float mathdivide(int a, int b);
 
+int a=6;int b=10;
 int main()
 {
+    printf("The addition of two numbers is %d \n",mathadd(a,b));
+    printf("The multiplication of two numbers is %d \n",mathmul(a,b));
+    printf("The multiplication of two numbers is %d \n",mathsub(a,b));
+    printf("The multiplication of two numbers is %f \n",mathdivide(a,b));
     printf("Test program \n");
 }
+
+int mathadd(int a, int b)
+{
+    int output=a+b;
+    return output;
+}
+
+int mathmul(int a, int b)
+{
+    int output=a*b;
+    return output;
+}
+
+int mathsub(int a, int b)
+{
+    int output=a-b;
+    return output;
+}
+
+float mathdivide(int a, int b)
+{
+    float output=((float) a)/b;
+    return output;
+}
+
 
