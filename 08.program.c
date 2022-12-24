802b24ca89019c6467c04323e3798fa8132af701
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-24 23:44:50 +0100

    modification

---
+++ 08.program.c
@@ -0,0 +1,23 @@
+// functions 
+
+#include <stdio.h>
+// void addnumbers(); // adds two values
+// void mulnumbers(int a,int b); // multiply two values
+//void sub() // subtracts two values
+
+void addnumbers(int a,int b);
+
+void main()
+{
+    addnumbers();
+    printf("The answer is %d \n",addnumbers);
+}
+
+void addnumbers(int a,int b)
+{
+    a=5;b=6;
+    int c;
+    // int b;
+    c=a+b;
+}
+
