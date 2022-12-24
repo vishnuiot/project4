16e859041d1fc4549f3f2270122564319f0fdb7a
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-24 22:27:52 +0100

    modification

--- 1.executecfile.sh
+++ 1.executecfile.sh
@@ -1,10 +1,11 @@
 #gcc -o test 1.program.c
 #gcc -o test 2.program.c
 #gcc -o test 3.program.c
-gcc -o test 4.program.c
+#gcc -o test 4.program.c
 #gcc -o test 5.program.c
 #gcc -o test 6.program.c 6a.function.c 
 #gcc -o test 7.program.c 7a.function.c
+gcc -o test 8.program.c
 #gcc -o test 9.program.c
 #gcc -o test 10.program.c
 #gcc -o test 100.practice_program.c 100a.practice_program.c
