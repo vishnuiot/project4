0b90eec11099315f627e4d29d286138e89730aad
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-25 00:00:21 +0100

    modification

--- 06c.program.c
+++ 06c.program.c
@@ -2,7 +2,14 @@
 
 #include <stdio.h>
 
+// list of functions
+int mathadd(int a, int b);
+int mathmul(int a,int b);
+int mathsub(int a,int b);
+int mathdivide(int a, int b);
+
 int main()
 {
     printf("Test program \n");
 }
+
