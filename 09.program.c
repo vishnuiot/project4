c2e12cc7ba97c6583b4e00ff84de5a98eccd0f05
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-26 20:56:44 +0100

    modification

--- 09.program.c
+++ 09.program.c
@@ -1,10 +1,4 @@
-//Example for pointer variables - read operation
-// create a char variable and initialize the value as 1500
-// print the address of the data 
-// create a pointer variable
-// store the address of the variable
-// performa the read operation to fetch 1 byte
-//
+//Example for pointer variables - read/write/substitute operation
 
 
 #include <stdio.h>
@@ -23,17 +17,6 @@ int main ()
     printf("The value of num is %d \n ",num);
     printf ("The value of the address of the integer data type is %d \n",num_ptr);
     printf("De_referencing of pointer value is %d \n",*num_ptr);
-    
-    char data =100;
-    char *pointervariable; 
-    pointervariable=&data;
-    // read operation
-    char value=*pointervariable; // dereference the pointer variable
-    printf("The address of the data %x \n",&data);
-    printf(" The value stored in pointer variable is  %d  \n",value);
 
-    // write operation
-    *pointervariable =60;
-    printf(" The value stored in pointer variable is  %d  \n",data);
-    return 0;
+
 }
