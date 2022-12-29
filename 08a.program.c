f3b80cceb45c5b60d2a6b4b2348b203d55779ba9
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-29 21:17:14 +0100

    modification

--- 08a.program.c
+++ 08a.program.c
@@ -1,12 +1,13 @@
 // conditional statements
+// 
 
 #include <stdio.h>
 
 int main()
 {
     int number=5;
-    if(number==5);
-    printf("THe number is 5 \n");
+    if(number==5)
+        printf("THe number is 5 \n");
 
     printf ("Test program \n");
 }
