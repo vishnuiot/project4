8a1f91235225e51f3d21eeeb3c0f2d3c4b7d9e9f
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-29 21:19:15 +0100

    modification

--- 08c.program.c
+++ 08c.program.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-
-    printf("Test program \n");
+    int number =5;
+    if (number ==5)
+    {
+        printf("THe number is %d \n",number);
+    }
+    // printf("Test program \n");
 }
