f2fdb1bdf5639db2d942b697a4a803400f83d522
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-29 21:39:51 +0100

    modification

--- 08d.program.c
+++ 08d.program.c
@@ -5,5 +5,18 @@
 
 int main()
 {
-    printf("Test program \n");
+    switch (3)
+    {
+    case 1:
+        printf("THe switch case is no 1 \n");
+        break;
+
+    case 3:
+        printf("The switch case function is no 3 \n");
+        break;
+    
+    default:
+        break;
+    }
+    // printf("Test program \n");
 }
