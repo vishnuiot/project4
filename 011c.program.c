f1e5d98a4751d970f76e69a3a9a56e974275a121
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-30 20:09:48 +0100

    modification

--- 011c.program.c
+++ 011c.program.c
@@ -7,11 +7,11 @@ int main()
     int j=0;
     for (i=0;i<5;i++)
     {
-        printf("THe value of i %d \n",i)
+        printf("THe value of i %d \n",i);
 
         for (j=0;j<3;j++)
         {
-            printf("The value of j is %d \n ",j)
+            printf("The value of j is %d \n ",j);
         }
     }
 
