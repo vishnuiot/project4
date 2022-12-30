ef5a6ec4a96e14787684054f4ef5e06430506531
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-30 20:02:50 +0100

    modification

--- 011b.program.c
+++ 011b.program.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main()
 {
-
-    printf("Test program \n");
+    int i=0;
+    for (i=0,i<10,i++)
+    {
+        printf("Test program - %d \n ",i);
+        i++;
+    }
+    
     return 0;
 }
