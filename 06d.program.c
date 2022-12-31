e5c8b120cc9eec025df8b3754e82dc368371380f
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-31 18:26:22 +0100

    modification

--- 06d.program.c
+++ 06d.program.c
@@ -1,10 +1,18 @@
 // Documeting a function
 
 #include <stdio.h>
+
+myaddition(int a,int b);            // Function prototype
 int main()
 {
-
+    myaddition(5,6);                // Function call
     printf("Test program \n ");
     return 0;
+}
+
+myaddition(int a,int b)             // function definition
+{
+    int result=a+b;
+    return result;                  // return statement
 }
 
