56bfe1975cd7d9167b0717bf2c40da79e281bc5b
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-31 19:22:22 +0100

    modification

--- 09.program.c
+++ 09.program.c
@@ -8,7 +8,7 @@ int main ()
     float num=20,num2=100;
     int *num_ptr;
     num_ptr=&num;
-    printf("The value of num is %e \n ",num);
+    printf("The value of num is %f \n ",num);
     printf("The address of num is %e \n",&num);
     printf ("The value of the address of the integer data type is %d \n",num_ptr);
     printf("De_referencing of pointer value is %d \n",*num_ptr);
