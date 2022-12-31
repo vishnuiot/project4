71010906a98644cdbd8ac943054bb88da134d86c
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-31 19:15:39 +0100

    modification

--- 09.program.c
+++ 09.program.c
@@ -21,6 +21,7 @@ int main ()
     *num_ptr=&num2;
     printf("The address of num2 is %d \n",*num_ptr);
     printf("Dereferencing num 2 %d \n",*num_ptr);
+    printf("The value of num is %d \n ",num2);
 
 
 
