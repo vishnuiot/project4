0cff4389670421f4d317b86065e3ff76f65d9e19
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-31 19:24:45 +0100

    modification

--- 09.program.c
+++ 09.program.c
@@ -14,14 +14,14 @@ int main ()
     printf("De_referencing of pointer value is %lf \n",*num_ptr);
 
     *num_ptr=50;
-    printf("The value of num is %e \n ",num);
-    printf ("The value of the address of the integer data type is %e \n",num_ptr);
-    printf("De_referencing of pointer value is %e \n",*num_ptr);
+    printf("The value of num is %lf \n ",num);
+    printf ("The value of the address of the integer data type is %d \n",num_ptr);
+    printf("De_referencing of pointer value is %d \n",*num_ptr);
 
     *num_ptr=&num2;
-    printf("The address of num2 is %e \n",*num_ptr);
-    printf("Dereferencing num 2 %e \n",*num_ptr);
-    printf("The value of num2 is %e \n ",num2);
+    printf("The address of num2 is %d \n",*num_ptr);
+    printf("Dereferencing num 2 %d \n",*num_ptr);
+    printf("The value of num2 is %lf \n ",num2);
 
 
 
