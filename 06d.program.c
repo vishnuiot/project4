36bfec1894143ce5a00cf3dd7a72072ff180dcf7
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-31 18:29:18 +0100

    modification

--- 06d.program.c
+++ 06d.program.c
@@ -6,7 +6,7 @@ myaddition(int a,int b);            // Function prototype
 int main()
 {
     int sum=myaddition(5,6);                // Function call
-    printf("The addition of two numbers is %d ",sum);       
+    printf("The addition of two numbers is %d \n",sum);       
     printf("Test program \n ");
     return 0;
 }
