bcf1fa8d35d48d6f7aaa289c888b0f70055ba772
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-31 18:52:10 +0100

    modification

--- 06d.program.c
+++ 06d.program.c
@@ -10,6 +10,7 @@ int main()
     float a,b,sum;
     sum=myaddition(5,6);                // Function call
     sum=myaddition(8.9,9);                // Function call
+    sum=myaddition(700.8,80);                // Function call
     sum=myaddition(700.8,80.2);                // Function call
     printf("Test program \n ");
     return 0;
