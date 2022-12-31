88a57ac6780f41a9807060982066b335db3ee65e
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-31 19:20:09 +0100

    modification

--- 09.program.c
+++ 09.program.c
@@ -8,20 +8,20 @@ int main ()
     float num=20,num2=100;
     float *num_ptr;
     num_ptr=&num;
-    printf("The value of num is %f \n ",num);
-    printf("The address of num is %f \n",&num);
-    printf ("The value of the address of the integer data type is %f \n",num_ptr);
-    printf("De_referencing of pointer value is %f \n",*num_ptr);
+    printf("The value of num is %e \n ",num);
+    printf("The address of num is %e \n",&num);
+    printf ("The value of the address of the integer data type is %e \n",num_ptr);
+    printf("De_referencing of pointer value is %e \n",*num_ptr);
 
     *num_ptr=50;
-    printf("The value of num is %f \n ",num);
-    printf ("The value of the address of the integer data type is %f \n",num_ptr);
-    printf("De_referencing of pointer value is %f \n",*num_ptr);
+    printf("The value of num is %e \n ",num);
+    printf ("The value of the address of the integer data type is %e \n",num_ptr);
+    printf("De_referencing of pointer value is %e \n",*num_ptr);
 
     *num_ptr=&num2;
-    printf("The address of num2 is %f \n",*num_ptr);
-    printf("Dereferencing num 2 %f \n",*num_ptr);
-    printf("The value of num2 is %f \n ",num2);
+    printf("The address of num2 is %e \n",*num_ptr);
+    printf("Dereferencing num 2 %e \n",*num_ptr);
+    printf("The value of num2 is %e \n ",num2);
 
 
 
