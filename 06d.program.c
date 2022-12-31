f098d0e37d8c3d13485eece1dac9390022d33a40
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-31 18:53:04 +0100

    modification

--- 06d.program.c
+++ 06d.program.c
@@ -10,7 +10,7 @@ int main()
     float a,b,sum;
     sum=myaddition(5,6);                // Function call
     sum=myaddition(8.9,9);                // Function call
-    sum=myaddition(double 700.8,80);                // Function call
+    sum=myaddition(700.8,80);                // Function call
     sum=myaddition(700.8,80.2);                // Function call
     printf("Test program \n ");
     return 0;
@@ -19,7 +19,7 @@ int main()
 float myaddition(float a,float b)             // function definition
 {
     float result=a+b;
-    printf("The addition of two numbers is %f \n",result); 
+    printf("The addition of two numbers is %d \n",result); 
     return result;                  // return statement
 }
 
