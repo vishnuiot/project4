d8d5f8928c9d51406b375a1b19fa20bc4b008d11
vishnuiot <vishnuprasad.iit@gmail.com>
2022-12-31 18:48:51 +0100

    modification

--- 06d.program.c
+++ 06d.program.c
@@ -7,7 +7,7 @@ int myaddition(int a,int b);            // Function prototype with return type i
 
 int main()
 {
-    int a,b,sum;
+    float a,b,sum;
     sum=myaddition(5,6);                // Function call
     sum=myaddition(8.9,9);                // Function call
     sum=myaddition(7,8);                // Function call
