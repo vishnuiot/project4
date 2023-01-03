8fd5c7cb15bd33a0605a6c0e0a8a75866185aae5
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-03 23:38:12 +0100

    modification

--- 07c.program.c
+++ 07c.program.c
@@ -9,14 +9,15 @@ int main()
     printf("Enter two numbers \n");
     scanf("%d, %d",&n1,&n2);
     printf("The entered numbers address are  %d & %d \n",n1,n2);
-
+    int sum=mathadd(n1,n2);
+    printf("The addition of two numbers is %d \n ",sum);
 }
 
     
 
 
-// int mathadd(int n1,int n2)
-// {
-//     int sum=n1+n2;
-//     return sum;
-// }
+int mathadd(int n1,int n2)
+{
+    int sum=n1+n2;
+    return sum;
+}
