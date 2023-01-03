99bcdbb4c4f0cb9b2128ff50dcdd8fda954b7758
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-03 23:28:27 +0100

    modification

--- 07c.program.c
+++ 07c.program.c
@@ -2,7 +2,20 @@
 
 #include<stdio.h>
 
+int mathadd(int n1,int n2);
+int result;
 int main()
 {
-    printf("Test program \n");
+    printf("Enter two numbers \n");
+    scanf("%d %d,&n1,&n2");
+    printf("The entered numbers address are \n");
+    result=mathadd(n1,n2);
+    printf("The sum of two numbers is %d ",result);
+    return 0;
+}
+
+int mathadd(int n1,int n2)
+{
+    int sum=n1+n2;
+    return sum;
 }
