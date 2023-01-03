3e9f439407db65ebe82f483a7ba6c131413983c6
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-03 23:33:53 +0100

    modification

--- 07c.program.c
+++ 07c.program.c
@@ -7,7 +7,7 @@ int n1,n2,result;
 int main()
 {
     printf("Enter two numbers \n");
-    scanf("%d %d",n1,n2);
+    scanf("%d, %d",n1,n2);
     printf("The entered numbers address are  %d & %d \n",n1,n2);
 
 }
@@ -15,8 +15,8 @@ int main()
     
 
 
-int mathadd(int n1,int n2)
-{
-    int sum=n1+n2;
-    return sum;
-}
+// int mathadd(int n1,int n2)
+// {
+//     int sum=n1+n2;
+//     return sum;
+// }
