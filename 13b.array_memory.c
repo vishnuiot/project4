8af5f811737a12b6d1d36b89c0977f643f1c9adf
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-06 14:12:03 +0100

    modification

--- 13b.array_memory.c
+++ 13b.array_memory.c
@@ -4,5 +4,11 @@
 #include<stdio.h>
 int main()
 {
-    printf(" Test program \n");
+    int a[5]={10,20,30,40,50};
+    int i=0;
+    for (i=0;i<5;i++)
+    {
+        printf("The value of i is %d \n",a[i]);
+    }
+    // printf(" Test program \n");
 }
