d00e8a19dbced6756aa45cb92479b0771bb9c37b
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-06 10:17:22 +0100

    modification

--- 12a.pointer.c
+++ 12a.pointer.c
@@ -5,9 +5,9 @@ int main()
 {
     int num=20;
     int *num_ptr;
-    num_ptr=num
+    num_ptr=num;
     printf("The value of num is %d ",num);
     printf("The address of num is %d",&num);
-    printf("The address of pointer is %d ",num_ptr)
+    printf("The address of pointer is %d ",num_ptr);
     // printf("test program \n");
 }
