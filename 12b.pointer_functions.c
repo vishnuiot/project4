209e486ae4a0294bf5165f1fb1f9a982201f22b1
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-06 11:11:38 +0100

    modification

--- 12b.pointer_functions.c
+++ 12b.pointer_functions.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
+
+int mathadd(int num1,int num2);
+int mathavg(int a, int b);
+a=7;b=10;
+
 int main()
 {
-    printf("Test program \n");
+    printf("The addition of two numbers is %d \n",mathadd(5,6));
+    return 0;
 }
+
+int mathadd(int num1,int num2)
+{
+    int result=num1+num2;
+    return result;
+}
+
+int mathavg(int a,int b)
+{
+    return (a/b);
+}
+
