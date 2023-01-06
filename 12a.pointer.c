2d6355e921fddca6232c444116ee9a4ed9c1120f
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-06 10:14:48 +0100

    modification

--- 12a.pointer.c
+++ 12a.pointer.c
@@ -1,5 +1,10 @@
+// pointer - datatype
+
 #include <stdio.h>
 int main()
 {
-    printf("test program \n");
+    int num;
+    printf("The value of num is %d ",num);
+    printf("The address of num is "&num);
+    // printf("test program \n");
 }
