401dc317de8ef09506c4326a9eab4de4f3c6f705
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-07 12:16:21 +0100

    modification

--- 13b.array_memory.c
+++ 13b.array_memory.c
@@ -9,6 +9,19 @@ int main()
     for (i=0;i<5;i++)
     {
         printf("The value of i is %d \n",a[i]);
+        printf("The address of i is %d \n",&a[i]);
     }
+
+    char b[5]={"a","b"};
+    int j=0;
+    for (j=0;j<5;j++)
+    {
+        printf("The value of j is %s \n",b[j]);
+        printf("The address of j is %d \n",&b[j]);
+
+
+    }
+
+    
     // printf(" Test program \n");
 }
