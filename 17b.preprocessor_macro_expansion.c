8fdee8cb0e054c95581f62243e79f4bd651d3554
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-09 15:41:20 +0100

    modification

--- 17b.preprocessor_macro_expansion.c
+++ 17b.preprocessor_macro_expansion.c
@@ -2,8 +2,12 @@
 
 #include<stdio.h>
 
+# define area(l,b) l*b
+
 int main()
 {
-    printf("Test program \n");
+    int length=100,breadth =20;
+
+    printf("The area is %d \n",area(length,breadth));
     return 0;
 }
