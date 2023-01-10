14ccdbb01bdea4cb634a864785edb0e637b47ab9
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-10 13:30:21 +0100

    modification

--- 01.program_printfstatements.c
+++ 01.program_printfstatements.c
@@ -7,9 +7,9 @@ int main()
     printf("c:\\Documents\\programming files \n");
     printf("Example for tab \t TAB executed\n");
     printf("Structure of a C program \n");
-    printf("Documentation section Ex : // program to calculate fuel efficiency\n");
-    printf("Link section Ex : #include <stdio.h>, #include<stdio.h>\n");
-    printf("Global Declaration Ex: int fueltype\n");
+    printf("Documentation section \tEx : // program to calculate fuel efficiency\n");
+    printf("Link section \tEx : #include <stdio.h>, #include<stdio.h>\n");
+    printf("Global Declaration \tEx: int fueltype\n");
     printf("main consisting of declation and execution part\n");
     return 0;
 }
