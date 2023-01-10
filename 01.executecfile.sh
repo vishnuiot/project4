5196beb85e27b52afbb316f3f2c3e55a853b61f5
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-10 13:26:50 +0100

    modification

--- 01.executecfile.sh
+++ 01.executecfile.sh
@@ -1,6 +1,6 @@
-#gcc -o test 01.program_printfstatements.c
+gcc -o test 01.program_printfstatements.c
 #gcc -o test 02.print_tabspace.c
-gcc -o test 03.data_types_long_float_double.c
+# gcc -o test 03.data_types_long_float_double.c
 #gcc -o test 04.type_casting.c
 #gcc -o test 05.static_external.c
 #gcc -o test 06.program.c 06a.function.c 
