4d3872734f6db5837d7bae8ee2308e385cc4308a
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-11 22:23:23 +0100

    modification

---
+++ 15e.bit_monitoring.c
@@ -0,0 +1,9 @@
+//  Monitor certain bits like flags in a register
+
+// During switch 1 --> ON and switch press again 0
+
+//  done using while condition
+
+// while (P0 &( 1<<5))
+// { do something
+// }
