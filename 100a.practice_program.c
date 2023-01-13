33131ac622f93b65387337a3a271e2527d656708
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-13 12:59:39 +0100

    modification

--- 100a.practice_program.c
+++ 100a.practice_program.c
@@ -2,15 +2,20 @@
 #include<stdio.h>
 int array[]={10,20,30,40,50};
 
-int i;
-
 int main()
 {
-for (i=0;i<0;i++)
-{
+
+for (int i=0;i<5;i++)
     printf("The array element no %d is %d \n",i,array[i]);
+
+int total=0;
+
+for (int i=0;i<5;i++)
+    total+=array[i];
+printf("The total is %d \n",total);
+printf("The average of array is %d \n",total/5);
     
-}
+
 }
 
     
--- 13b.array_memory.c
+++ 13b.array_memory.c
@@ -4,21 +4,29 @@
 #include<stdio.h>
 int main()
 {
-    int a[5]={10,20,30,40,50};
+    int a[]={10,20,30,40,50};
     int i=0;
     for (i=0;i<5;i++)
     {
-        printf("The value of i is %d \n",a[i]);
-        printf("The address of i is %d \n",&a[i]);
+        printf("The value of i is %d  and address of i is %d \n",a[i],&a[i]);
+        // printf("The address of i is %d \n",&a[i]);
     }
 
-    char b[5]={"a","b"};
-    int j=0;
+// array operations
+    int array[]={100,200,300,400,500};
+    int total=0;
+
+    for (int i=0;i<5;i++)
+    total+=array[i];
+    printf("The total is %d \n",total);
+    printf("The average of array is %d \n",total/5);
+// character operations
+    char b[5]={"a""b""c""d""e"};
+    int j;
     for (j=0;j<5;j++)
     {
-        printf("The value of j is %s \n",b[j]);
-        printf("The address of j is %d \n",&b[j]);
-
+        printf("The value of j is %c  and address of j is %d \n",b[j],&b[j]);
+        // printf("The address of j is %d \n",&b[j]);
 
     }
 
