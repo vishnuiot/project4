d31f217890ba281b0db48a252b2ea99e6b5d551a
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-14 23:11:01 +0100

    modification

--- 100.practice_program.c
+++ 100.practice_program.c
@@ -5,7 +5,7 @@
 int main()
 {
     
-    switch (1){
+    switch (2){
     case(1):printf("The swithc case is 1 \n");break;
     case (2): printf("the switch case is 2 \n");break;
     default: break;
