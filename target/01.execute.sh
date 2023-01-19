370049431b64c8cb36d2adcd4f781295c00cee53
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-19 12:48:11 +0100

    modification

--- target/01.execute.sh
+++ target/01.execute.sh
@@ -1,2 +1,4 @@
-gcc -o test 01.running_led.c
+# gcc -o test 01.running_led.c
+# gcc -o test 02.countdown_counter.c
+gcc -o test 03.binary_countdown.c
 
---
+++ target/02.countdown_counter.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main()
+{
+    int i=0;
+    for (i=0;i<255;i++)
+    {
+        printf("The count down number is = %d \n",i);
+
+    }
+    return 0;
+
+}
