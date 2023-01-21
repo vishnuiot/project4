ec3121786c5884c46b3c2a64725755b7d7da77ae
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-21 21:58:18 +0100

    modification

--- 12b.pointer_functions.c
+++ 12b.pointer_functions.c
@@ -5,11 +5,13 @@
 int main()
 {
     int a=1,b=2,c=3;
-    printf("The answer is %d \n",addone(&a,&b,&c));
+    
     return 0;
 }
 
 int addone(int *a,int *b,int *c)
 {
 *a=*a+1;
+printf("The answer is %p \n",addone(&a,&b,&c));
+
 }
