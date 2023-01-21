718982fa12cd661ba4449d527489d731c4543dbc
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-21 20:54:26 +0100

    modification

--- 06b.two_functions.c
+++ 06b.two_functions.c
@@ -1,8 +1,21 @@
 // Example for two functions
 
 #include <stdio.h>
+#include<math.h>
+
+float power(float a,float b);
+
 int main()
 {
-    printf("Test program \n");
+    float x;
+    x=power(10,2);
+    printf("The answer is %f \n",x);
     return 0;
 }
+
+float power(float a,float b)
+{
+    int result = a+b;
+    return result;
+}
+
