75e1fa95503e88e6d2c82690fc5e4a2ea421d4a7
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-24 21:51:09 +0100

    modification

--- 01_Embedded_C_Fundamentals/07e.function_float.c
+++ 01_Embedded_C_Fundamentals/07e.function_float.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 
-float newfunc(float a,float b);
+float newfunc(float a, float b);
+float power(float x, int n);
 int main()
 {
-    float x=5;
-    float g=newfunc(10,5);
-    printf("G is %f \n",g);
-    
+    float x = 5;
+    float g = power(5.8, 9);
+    printf("The answer is  %0.2f \n", g);
+
     return 0;
 }
 
-float newfunc(float a,float b)
+float newfunc(float a, float b)
 {
     float c;
-    c= a*b;
-    printf("The answer is %f \n",c);
+    c = a * b;
+    // printf("The answer is %f \n", c);
     return c;
+}
+
+float power(float x, int n)
+{
+    int i;
+    int result = x;
+    for (i = 1; i < n; i++)
+    {
+        result=newfunc(result,x);
+    }
+    return result;
 }
