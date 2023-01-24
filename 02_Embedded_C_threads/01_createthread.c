0176891918692b49a1850a9fa2a4d7efb5223184
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-24 16:54:06 +0100

    modification

--- 02_Embedded_C_threads/01_createthread.c
+++ 02_Embedded_C_threads/01_createthread.c
@@ -1,8 +1,15 @@
-# include <stdio.h>
-# include<stdint.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void routine()
+{
+    printf("Test program \n");
+}
 
 int main()
 {
     printf("Test program \n ");
+    return 0;
 }
