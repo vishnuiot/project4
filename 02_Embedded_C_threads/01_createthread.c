2949b88563ca6e29a350017d3de131ab20db7c23
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-24 17:37:37 +0100

    modification

--- 02_Embedded_C_threads/01_createthread.c
+++ 02_Embedded_C_threads/01_createthread.c
@@ -3,13 +3,24 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void routine()
+void *routine()
 {
     printf("Test program \n");
+    sleep(3);
+    printf("Message ending thread \n");
+
+
 }
 
 int main()
 {
-    printf("Test program \n ");
+    // thread one
+    pthread_t t1;
+    // thread two
+    pthread_t t2;
+    pthread_create(&t1, NULL, &routine, NULL);
+    pthread_create(&t2, NULL, &routine, NULL);
+    pthread_join(t1, NULL);    
+    pthread_join(t2, NULL);
     return 0;
 }
