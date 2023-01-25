1bfea82dc64aee690107198dc729da00747c88d1
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-25 23:07:52 +0100

    modification

--- 01_Embedded_C_Fundamentals/09.intro2pointers.c
+++ 01_Embedded_C_Fundamentals/09.intro2pointers.c
@@ -1,30 +1,34 @@
-//Example for pointer variables - read/write/substitute operation
-// https://stackoverflow.com/questions/1689423/memory-address-positive-or-negative-value-in-c
-
+// Example for pointer variables - read/write/substitute operation
+//  https://stackoverflow.com/questions/1689423/memory-address-positive-or-negative-value-in-c
 
 #include <stdio.h>
-int main ()
+int main()
 {
-    
-    float num=20,num2=100;
+    // pointer practice program
+
+    int a = 10;
+    int *p;
+    p = &a;
+    printf("THe value stores by pointer p is %p \n", &a);
+    *p = *p + 1;
+    printf("The value of a now is %d \n", a);
+    return 0;
+
+    float num = 20, num2 = 100;
     int *num_ptr;
-    num_ptr=&num;
-    printf("The value of num is %lf \n ",num);
-    printf("The address of num is %p \n",&num);
-    printf ("The value of the address of the integer data type is %p \n",num_ptr);
-    printf("De_referencing of pointer value is %lf \n",*num_ptr);
+    num_ptr = &num;
+    printf("The value of num is %lf \n ", num);
+    printf("The address of num is %p \n", &num);
+    printf("The value of the address of the integer data type is %p \n", num_ptr);
+    printf("De_referencing of pointer value is %lf \n", *num_ptr);
 
-    *num_ptr=50;
-    printf("The value of num is %lf \n ",num);
-    printf ("The value of the address of the integer data type is %p \n",num_ptr);
-    printf("De_referencing of pointer value is %p \n",*num_ptr);
+    *num_ptr = 50;
+    printf("The value of num is %lf \n ", num);
+    printf("The value of the address of the integer data type is %p \n", num_ptr);
+    printf("De_referencing of pointer value is %p \n", *num_ptr);
 
-    *num_ptr=&num2;
-    printf("The address of num2 is %d \n",*num_ptr);
-    printf("Dereferencing num 2 %d \n",*num_ptr);
-    printf("The value of num2 is %lf \n ",num2);
-
-
-
-
+    *num_ptr = &num2;
+    printf("The address of num2 is %d \n", *num_ptr);
+    printf("Dereferencing num 2 %d \n", *num_ptr);
+    printf("The value of num2 is %lf \n ", num2);
 }
--- miniprojects/project1_guess_no_game/01.executefile.sh
+++ miniprojects/project1_guess_no_game/01.executefile.sh
@@ -1,1 +1,8 @@
 gcc -o test 01.guessno_game.c
+gcc -o test 02.calculate_power_of_number.c
+
+
+./test
+# cd ..
+# ./gitupload.sh
+# cd -
