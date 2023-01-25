371773f9086cf7e8cba0df2b6d6ca8255f151b5a
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-25 23:48:19 +0100

    modification

--- miniprojects/02_project_phonebook/01.phonebook.c
+++ miniprojects/02_project_phonebook/01.phonebook.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
 {
+    char n[50];
+    int a,p,d;
+    char add[50];
+    char na[50];
+    int ag,ph,da;
+    char add[50];
+
     printf("Test program \n");
     return 0;
 }
