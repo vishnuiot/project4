eba775768986cb76ff38788530320199a378894e
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-26 23:37:55 +0100

    modification

--- 03_Embedded_C_processess/04.fork_wait.c
+++ 03_Embedded_C_processess/04.fork_wait.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 int main()
 {
-    printf("test program \n");
+    int id = fork();
+    int n;
+    if (id == 0)
+    {
+        n = 1;
+    }
+    else
+    {
+        n = 6;
+    }
+    if (id != 0)
+    {
+        wait();
+    }
+
+    for (int i = 0; i < n+5 ; i++)
+    {
+        printf("%d", i);
+        fflush(stdout);
+    }
+
+    printf("\n");
     return 0;
-    
 }
