780786e31e9af047b68a75025125f4a9fca5a7c1
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-26 12:36:30 +0100

    modification

--- 01_Embedded_C_Fundamentals/13i.array_memoryaddress.c
+++ 01_Embedded_C_Fundamentals/13i.array_memoryaddress.c
@@ -1,9 +1,17 @@
-//  array address
+//  passing the memory array address
 
+int addone(int array[],int y);
 #include <stdio.h>
 int main()
 {
     int a[] = {1,2,3,4,5};
     printf("a:%p \n",a);
+    addone(a,4);
+    return 0;
+}
+
+int addone(int array[],int y)
+{
+    printf("The array address is %p \n",array);
     return 0;
 }
