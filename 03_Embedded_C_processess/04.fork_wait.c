905abd5c08da0a35d22fe152102c8e828b442f1c
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-27 20:34:18 +0100

    modification

--- 03_Embedded_C_processess/04.fork_wait.c
+++ 03_Embedded_C_processess/04.fork_wait.c
@@ -11,7 +11,7 @@ int main()
     }
     else
     {
-        n = 6;
+        n = 10;
     }
     // check to figure out if we are in main process
     if (id != 0)
@@ -19,12 +19,24 @@ int main()
         wait();
     }
 
-    for (int i = 0; i < n+5 ; i++)
+    for (int i = 0; i < n + 5; i++)
     {
-        printf("The child process id is %d - %d \n", id,i);
-        fflush(stdout);
+        if (id == 0)
+        {
+            printf("The child process id is %d - %d \n", id, i);
+            fflush(stdout);
+        }
+        else
+        {
+            printf("The main process id is %d - %d \n", id, i);
+            fflush(stdout);
+        }
     }
 
-    printf("\n");
+    if (id != 0)
+    {
+        printf("\n");
+    }
+
     return 0;
 }
